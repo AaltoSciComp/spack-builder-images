09d8a30a388becb8f82572287348c11233ea78c5
Simo Tuomisto <simo.tuomisto@aalto.fi>
2017-11-28 10:58:41 +0200

    Added lmod module paths to shell.

--- dockerfiles/ubuntu/entry.sh
+++ dockerfiles/ubuntu/entry.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-if [ -e /usr/share/lmod/5.8/init/bash ]; then
-  source /usr/share/lmod/5.8/init/bash
-fi
-if [ -e /opt/spack/share/spack/setup-env.sh ]; then
-  source /opt/spack/share/spack/setup-env.sh
-fi
+[[ -f /opt/spack/share/spack/setup-env.sh ]] && . /opt/spack/share/spack/setup-env.sh
 
+[[ -f /usr/share/lmod/5.8/init/bash ]] && . /usr/share/lmod/5.8/init/bash
+
+_python_command=$(printf  "%s\\\n%s\\\n%s" \
+"print(\'_sp_lmod_root={0}\'.format(spack.util.path.canonicalize_path(spack.config.get_config(\'config\').get(\'module_roots\', {}).get(\'lmod\'))))"
+)
+
+_assignment_command=$(spack-python -c "exec('${_python_command}')")
+
+eval ${_assignment_command}
+
+module use "${_sp_lmod_root%/}/$_sp_sys_type"
+
+[[ -f /opt/local/etc/bash_completion ]] && . /opt/local/etc/bash_completion
 
 if [[ "$#" -eq 0 ]]; then 
   cd /build
