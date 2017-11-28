33295412803fd88c3c99d25e9e7fadb336de88c1
Simo Tuomisto <simo.tuomisto@aalto.fi>
2017-11-28 13:28:23 +0200

    Fixed tcl module path.

--- dockerfiles/ubuntu/entry.sh
+++ dockerfiles/ubuntu/entry.sh
@@ -5,6 +5,7 @@
 [[ -f /usr/share/lmod/5.8/init/bash ]] && . /usr/share/lmod/5.8/init/bash
 
 _python_command=$(printf  "%s\\\n%s\\\n%s" \
+"print(\'_sp_tcl_root={0}\'.format(spack.util.path.canonicalize_path(spack.config.get_config(\'config\').get(\'module_roots\', {}).get(\'tcl\'))))" \
 "print(\'_sp_lmod_root={0}\'.format(spack.util.path.canonicalize_path(spack.config.get_config(\'config\').get(\'module_roots\', {}).get(\'lmod\'))))"
 )
 
@@ -12,6 +13,7 @@ _assignment_command=$(spack-python -c "exec('${_python_command}')")
 
 eval ${_assignment_command}
 
+module unuse "${_sp_tcl_root%/}/$_sp_sys_type"
 module use "${_sp_lmod_root%/}/$_sp_sys_type"
 
 [[ -f /opt/local/etc/bash_completion ]] && . /opt/local/etc/bash_completion
