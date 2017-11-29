cb6b87003f6331d8f3d37a96d9957a2a08e5b9bf
Simo Tuomisto <simo.tuomisto@aalto.fi>
2017-11-29 16:00:10 +0200

    Modified path loadings to fix module issues.

--- dockerfiles/ubuntu/entry.sh
+++ dockerfiles/ubuntu/entry.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
+#export LMOD_RC=/etc/lmod.lmodrc.lua
+
 [[ -f /opt/spack/share/spack/setup-env.sh ]] && . /opt/spack/share/spack/setup-env.sh
-
-[[ -f /usr/share/lmod/5.8/init/bash ]] && . /usr/share/lmod/5.8/init/bash
-
-_python_command=$(printf  "%s\\\n%s\\\n%s" \
-"print(\'_sp_tcl_root={0}\'.format(spack.util.path.canonicalize_path(spack.config.get_config(\'config\').get(\'module_roots\', {}).get(\'tcl\'))))" \
-"print(\'_sp_lmod_root={0}\'.format(spack.util.path.canonicalize_path(spack.config.get_config(\'config\').get(\'module_roots\', {}).get(\'lmod\'))))"
-)
-
-_assignment_command=$(spack-python -c "exec('${_python_command}')")
-
-eval ${_assignment_command}
-
-module unuse "${_sp_tcl_root%/}/$_sp_sys_type"
-module use "${_sp_lmod_root%/}/$_sp_sys_type"
 
 [[ -f /opt/local/etc/bash_completion ]] && . /opt/local/etc/bash_completion
 
@@ -23,5 +11,17 @@ if [[ "$#" -eq 0 ]]; then
   python install.py
   exit $?
 else
+  [[ -f /usr/share/lmod/5.8/init/bash ]] && . /usr/share/lmod/5.8/init/bash
+
+  _python_command=$(printf  "%s\\\n%s\\\n%s" \
+  "print(\'_sp_tcl_root={0}\'.format(spack.util.path.canonicalize_path(spack.config.get_config(\'config\').get(\'module_roots\', {}).get(\'tcl\'))))" \
+  "print(\'_sp_lmod_root={0}\'.format(spack.util.path.canonicalize_path(spack.config.get_config(\'config\').get(\'module_roots\', {}).get(\'lmod\'))))"
+  )
+
+  _assignment_command=$(spack-python -c "exec('${_python_command}')")
+  eval ${_assignment_command}
+  module unuse "${_sp_tcl_root%/}/$_sp_sys_type"
+  module use "${_sp_lmod_root%/}/$_sp_sys_type"
+
   exec "$@"
 fi
